b79be0657fe233388996875fc8576992fd4d2b41
Adam Kariv <adam.kariv@gmail.com>
2021-08-21 22:25:37 +0300

    Integrate md with translations

--- tools/to_markdown.py
+++ tools/to_markdown.py
@@ -3,6 +3,8 @@ import yaml
 def recurse_into_taxonomy(items, output, level=0):
     for item in items:
         name = item['name']
+        if isinstance(name, dict):
+            name = name.get('tx', dict()).get('he') or name.get('source')
         if level == 0:
             output.write('# {}\n'.format(name))
         elif level == 1:
@@ -21,8 +23,11 @@ def recurse_into_taxonomy(items, output, level=0):
             recurse_into_taxonomy(item.get('items'), output, level+1)
 
 if __name__ == '__main__':
-    taxonomy = yaml.load(open('taxonomy.yaml'))
+    taxonomy = yaml.load(open('taxonomy.tx.yaml'))
     with open('TAXONOMIES.md', 'w') as output:
+        output.write('<div dir="rtl">\n')
         output.write('# טקסונומיית המענים הפתוחים\n\n')
 
         recurse_into_taxonomy(taxonomy, output)
+
+        output.write('</div>\n')
