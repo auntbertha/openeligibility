8cbd5cfb75f3df270eda35656d3f310798cc2331
Adam Kariv <adam.kariv@gmail.com>
2021-08-21 22:42:19 +0300

    Better formatting for the markdown

--- tools/to_markdown.py
+++ tools/to_markdown.py
@@ -6,9 +6,9 @@ def recurse_into_taxonomy(items, output, level=0):
         if isinstance(name, dict):
             name = name.get('tx', dict()).get('he') or name.get('source')
         if level == 0:
-            output.write('# {}\n'.format(name))
-        elif level == 1:
             output.write('## {}\n'.format(name))
+        elif level == 1:
+            output.write('### {}\n'.format(name))
         elif level == 2:
             output.write('- {}\n'.format(name))
         elif level == 3:
@@ -25,7 +25,7 @@ def recurse_into_taxonomy(items, output, level=0):
 if __name__ == '__main__':
     taxonomy = yaml.load(open('taxonomy.tx.yaml'))
     with open('TAXONOMIES.md', 'w') as output:
-        output.write('<div dir="rtl">\n')
+        output.write('<div dir="rtl">\n\n')
         output.write('# טקסונומיית המענים הפתוחים\n\n')
 
         recurse_into_taxonomy(taxonomy, output)
