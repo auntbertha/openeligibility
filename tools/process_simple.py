fd09c93f1a045e84bd09676c7b32599e2ae054d7
Adam Kariv <adam.kariv@gmail.com>
2021-10-25 15:17:44 +0300

    Extract description from simple file

--- tools/process_simple.py
+++ tools/process_simple.py
@@ -4,23 +4,39 @@ from slugify import slugify
 def to_slug(path):
     return ':'.join(slugify(p, separator='_', to_lower=True) for p in path)
 
+def split_description(name):
+    parts = name.split('||')
+    if len(parts) == 1:
+        return name.strip(), None
+    else:
+        return parts[0].strip(), parts[1].strip()
+
 def process_items(nodes, path=[]):
     ret = []
     for node in nodes:
         if isinstance(node, dict):
             name = list(node.keys())[0]
+            name, description = split_description(name)
             _path = path + [name]
-            ret.append(dict(
+            node = dict(
                 name=name,
                 slug=to_slug(_path),
                 items=process_items(node[name], _path)
-            ))
+            )
+            if description:
+                node['description'] = description
+            ret.append(node)
         elif isinstance(node, str):
-            _path = path + [node]
-            ret.append(dict(
-                name=node,
+            name = node
+            name, description = split_description(name)
+            _path = path + [name]
+            node = dict(
+                name=name,
                 slug=to_slug(_path),
-            ))
+            )
+            if description:
+                node['description'] = description
+            ret.append(node)
     return ret
 
 if __name__ == '__main__':
